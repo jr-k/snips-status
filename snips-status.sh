71dee2fcd30c3bde6be455836b6cabb97d807c5a
JRK <jessym.reziga@gmail.com>
2019-01-24 18:51:19 +0100

    Update snips-status.sh

--- snips-status.sh
+++ snips-status.sh
@@ -1,12 +1,13 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
 NC='\033[0m'
 
 linuxStatusCheck() {
-	X=$(sudo systemctl status snips-* | grep -i -E 'Active|●')
-	
+	X=$(sudo systemctl status $1 | grep -i -E 'Active|●')
+
 	statusIsNext=false
-	
+
 	for i in $(echo $X | tr "●" "\n")
 	do
 	        if [[ $i =~ \.service$ ]]; then
@@ -14,17 +15,29 @@ linuxStatusCheck() {
 	                printf "[Service] "
 	                printf '%-25s' "$service: "
 	        fi
-	
+
 	        if $statusIsNext ; then
-	                if [[ $i =~ active$ ]]; then
-	                        printf "${GREEN}[Active]${NC}\n"
-	                else
-	                        printf "${RED}[Inactive]${NC}\n"
-	                fi
-	
+			case "$i" in
+				active*)
+					printf "${GREEN}[Active]${NC}\n"
+				;;
+				activating*)
+					printf "${ORANGE}[Activating]${NC}\n"
+				;;
+				inactive*)
+					printf "${RED}[Inactive]${NC}\n"
+				;;
+				failed*)
+					printf "${RED}[Inactive]${NC}\n"
+				;;
+				*)
+					printf "${NC}[Unknown:$i]${NC}\n"
+				;;
+			esac
+
 	                statusIsNext=false
 	        fi
-	
+
 	        if [[ $i =~ Active: ]]; then
 	                statusIsNext=true
 	        fi
@@ -32,19 +45,17 @@ linuxStatusCheck() {
 }
 
 macosStatusCheck() {
-	X=$(brew services list | grep snips-)
-	
-	brew services list | grep snips- | while read -r line ; do
+	brew services list | grep $1 | while read -r line ; do
 	        cpt=0
 	        for i in $(echo $line | tr " " "\n")
 	        do
 	                cpt=$((cpt+1))
-	
+
 	                if [[ cpt -eq 1 ]] ; then
 	                        printf "[Service] "
 	                        printf '%-25s' "$i: "
 	                fi
-	
+
 	                if [[ cpt -eq 2 ]] ; then
 	                        if [[ $i = started$ ]] ; then
 	                                printf "${GREEN}[Active]${NC}\n"
@@ -53,13 +64,25 @@ macosStatusCheck() {
 	                        fi
 	                fi
 	        done
-	done	
+	done
 }
 
 case "$OSTYPE" in
-  darwin*)  macosStatusCheck ;; 
-  cygwin*)     printf "${RED}Windows systems are not supported yet${NC}" ;;
-  msys*)     printf "${RED}Windows systems are not supported yet${NC}" ;;
-  win32*)     printf "${RED}Windows systems are not supported yet${NC}" ;;
-  *)        linuxStatusCheck ;;
+	darwin*)
+		macosStatusCheck "mosquitto"
+		macosStatusCheck "snips-"
+	;;
+	cygwin*)
+		printf "${RED}Windows systems are not supported yet${NC}"
+	;;
+	msys*)
+		printf "${RED}Windows systems are not supported yet${NC}"
+	;;
+	win32*)
+		printf "${RED}Windows systems are not supported yet${NC}"
+	;;
+	*)
+		linuxStatusCheck "mosquitto"
+		linuxStatusCheck "snips-*"
+	;;
 esac
